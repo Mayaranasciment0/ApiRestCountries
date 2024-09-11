d52bf50f7f5cf91ab94db2fc652e34ba93af4f10
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-09-10 22:57:49 -0300

    Criando o MoedaService.java

---
+++ src/main/java/com/example/demo/service/MoedaService.java
@@ -0,0 +1,50 @@
+package main.java.com.example.demo.service;
+
+import main.java.com.example.demo.model.Moeda;
+import main.java.com.example.demo.repository.MoedaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MoedaService {
+
+    @Autowired
+    private MoedaRepository moedaRepository;
+
+    public List<Moeda> getAllMoedas() {
+        return moedaRepository.findAll();
+    }
+
+    public Optional<Moeda> getMoedaById(Long id) {
+        return moedaRepository.findById(id);
+    }
+
+    public Moeda saveMoeda(Moeda moeda) {
+        return moedaRepository.save(moeda);
+    }
+
+    public Moeda updateMoeda(Long id, Moeda moedaAtualizada) {
+        return moedaRepository.findById(id).map(moeda -> {
+            moeda.setNome(moedaAtualizada.getNome());
+            moeda.setSimbolo(moedaAtualizada.getSimbolo());
+            moeda.setCodigo(moedaAtualizada.getCodigo());
+            moeda.setPais(moedaAtualizada.getPais());
+            return moedaRepository.save(moeda);
+        }).orElseThrow(() -> new RuntimeException("Moeda não encontrada com o ID: " + id));
+    }
+
+    public void deleteMoeda(Long id) {
+        if (moedaRepository.existsById(id)) {
+            moedaRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Moeda não encontrada com o ID: " + id);
+        }
+    }
+
+    public Optional<Moeda> findByCodigo(String codigo) {
+        return moedaRepository.findByCodigo(codigo);
+    }
+}
