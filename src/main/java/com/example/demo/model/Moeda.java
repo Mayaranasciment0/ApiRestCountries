fd3df0ec411defa97ee2e07cac95c38ef2282752
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-09-10 22:41:51 -0300

    criando o model Moeda.java

---
+++ src/main/java/com/example/demo/model/Moeda.java
@@ -0,0 +1,91 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+//import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Moeda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String simbolo;
+    private String codigo;
+
+    @ManyToOne
+    @JoinColumn(nome = "pais_id")
+    private Pais pais;
+
+    public Moeda() {
+    }
+
+    public Moeda(String nome, String simbolo, String codigo, Pais pais) {
+        this.nome = nome;
+        this.simbolo = simbolo;
+        this.codigo = codigo;
+        this.pais = pais;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+    
+    @Override
+    public String toString() {
+        return "Moeda{" +
+                "id=" + id +
+                ", codigo='" + codigo + '\'' +
+                ", nome='" + nome + '\'' +
+                ", simbolo='" + simbolo + '\'' +
+                ", pais=" + (pais != null ? pais.getNome() : "null") +
+                '}';
+    }
+}
